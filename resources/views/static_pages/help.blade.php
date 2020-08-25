5a7fc79a88728dac1a9ca74f6af33d6680d9347f
kl078101 <kl078101@qq.com>
2020-08-25 03:55:11 +0000

    基础页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', '帮助')
+
+@section('content')
+  <h1>帮助有</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','StaicPagesController@home');
+Route::get('/help','StaicPagesController@help');
+Route::get('/about','StaicPagesController@about');
